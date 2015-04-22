3ffe875d98224291c45ed13da7a980db8b356a12
Mario Andrade <masterxoriam@gmail.com>
2015-04-21 23:09:12 -0700

    All three heuristics are working for the term project

--- 8-puzzle.py
+++ 8-puzzle.py
@@ -26,7 +26,7 @@ def generate_children(current):
 		children.append(init_node(left, current, "left", current.depth + 1, 0))
 
 	#generate the right move, if not on right-most side
-	if current_location != 2 and current_location != 5 and current_location != 8:
+	if current_location % 3 < 2:
 		#right-most indices are 2, 5, 8, so we create a child if we are not
 		#in any of these
 		right_index = current_location + 1
@@ -35,7 +35,7 @@ def generate_children(current):
 		children.append(init_node(right, current, "right", current.depth + 1, 0))
 
 	#generate the down move, if not on the bottom-most tiles
-	if current_location < 5:
+	if current_location < 6:
 		#bottom-most indices are greater than 5, so we can just exclude them
 		bottom_index = current_location + 3
 		down = current.state[:]
@@ -55,55 +55,108 @@ def in_place_selection_sort(L):
 	#Algorithm takes the lowest heuristic value and puts it in the first spot of the array.
 	#Each successive will be the next lowest until the algorithm reaches the k-1 spot. 
 	for k in range (len(L)-1):
-		least = k
+		greatest = k
 		for i in range (k+1, len(L)):
-			if L[i].heuristic_value < L[least].heuristic_value:
-				least = i
+			if L[i].cost > L[greatest].cost:
+				greatest = i
 		#swap elements
-		L[k], L[least] = L[least], L[k]
+		L[k], L[greatest] = L[greatest], L[k]
 	return L
 
 
+def tile_reversal(current, goal):
+	reversal = 0
+	for i in range(1,9):
+		current_index = current.state.index(i)
+		goal_index = goal.index(i)
+
+		# Check all directions to see if there is a reversal
+		if current_index > 2:
+			# Checking up
+			if current_index - 3 == goal_index:
+				# Candidate for reversal. Now to check if the candidate is also reversed.
+				candidate = current.state[goal_index]
+				if candidate == goal[current_index]:
+					# We have a match
+					reversal += 1
+
+		if current_index % 3 != 0:
+			# Checking left
+			if current_index - 1 == goal_index:
+				# Candidate for reversal. Now to check if the candidate is also reversed.
+				candidate = current.state[goal_index]
+				if candidate == goal[current_index]:
+					# We have a match
+					reversal += 1
+
+		if current_index % 3 < 2:
+			# Checking right
+			if current_index + 1 == goal_index:
+				# Candidate for reversal. Now to check if the candidate is also reversed.
+				candidate = current.state[goal_index]
+				if candidate == goal[current_index]:
+					# We have a match
+					reversal += 1
+	
+		if current_index < 6:
+			# Checking up
+			if current_index + 3 == goal_index:
+				# Candidate for reversal. Now to check if the candidate is also reversed.
+				candidate = current.state[goal_index]
+				if candidate == goal[current_index]:
+					# We have a match
+					reversal += 1
+	return 2 * reversal
+
+
+
 def sum_of_distances_out_of_place(current, goal):
-	heuristic_value = 0
+	cost = 0
 	for i in range(1, 9):
 		# Reference from:
 		# http://www.cyotek.com/blog/converting-2d-arrays-to-1d-and-accessing-as-either-2d-or-1d
+		# Got idea, but it didn't calculate right. Below comment was used to derive x and y values
+		# i:0 1 2 3 4 5 6 7 8
+		# x:0 1 2 0 1 2 0 1 2
+		# y:0 0 0 1 1 1 2 2 2
 		current_index = current.state.index(i)
-		cy = current_index / 3
-		cx = current_index - (cy * 3)
+		cy = current_index % 3
+		cx = int (current_index / 3) % 3
 		
 		goal_index = goal.index(i)
-		gy = goal_index / 3
-		gx = goal_index - (cy * 3)
-		#Reference from:
-		#http://stackoverflow.com/questions/16318757/calculating-manhattan-distance-in-python-in-an-8-puzzle-game
-		heuristic_value += abs(cx - gx) + abs(cy - gy)
-	return heuristic_value + current.depth
+		gy = goal_index % 3
+		gx = int (goal_index / 3) % 3
+		# Reference from:
+		# http://stackoverflow.com/questions/16318757/calculating-manhattan-distance-in-python-in-an-8-puzzle-game
+		cost += abs(cx - gx) + abs(cy - gy)
+	return cost + current.depth
 	
+
 def tiles_out_of_place(current, goal):
 	#Assign a heuristic value to each by comparing child to goal state
-	heuristic_value = 0 
+	out_of_place = 0 
 	#heuristic value goes up for each cv and gv that don't match
 	#cv = current value and gv = goal value
 	for cv, gv in zip(current.state, goal):
 		if(cv != gv):
 			if(cv != 0):
 				#Ignores the blank as it is not a tile.
-				heuristic_value += 1
-	return heuristic_value + current.depth
+				out_of_place += 1
+	return out_of_place + current.depth
 
 
-def calculate_heuristic_value(current, goal, choice):
-	heuristic_value = 0
+def calculate_cost(current, goal, choice):
+	cost = 0
 	if choice == 1:
-		heuristic_value = tiles_out_of_place(current, goal)
-		return heuristic_value
+		cost = tiles_out_of_place(current, goal)
+		return cost
 	elif choice == 2:
-		heuristic_value = sum_of_distances_out_of_place(current, goal)
-		return heuristic_value
+		cost = sum_of_distances_out_of_place(current, goal)
+		return cost
 	elif choice == 3:
-		pass
+		cost = sum_of_distances_out_of_place(current, goal)
+		reversal = tile_reversal(current, goal)
+		return current.depth + cost + reversal
 	else:
 		sys.exit(1)
 	
@@ -118,8 +171,10 @@ def best_first_search(start, goal, choice):
 	#Closed state is initiated empty
 	closed_states = []
 
+	loop = 0
 	#The loop that controls the entire best-first search algorithm
 	while open_states != []:
+		loop += 1
 		#Pop the front most value of the list, as it will be the one with the lowest heuristic value
 		current = open_states.pop()
 
@@ -147,19 +202,19 @@ def best_first_search(start, goal, choice):
 						index = closed_states.index(node)
 						break
 
-				child.heuristic_value = calculate_heuristic_value(child, goal, choice)
+				child.cost = calculate_cost(child, goal, choice)
 
 				if exists_in_open == False and exists_in_closed == False:
 					#child does not exists in both states
 					open_states.append(child)
 				elif exists_in_open == True and exists_in_closed == False:
 					#child exists in open states
-					if child.heuristic_value < open_states[index].heuristic_value:
+					if child.cost < open_states[index].cost:
 						open_states.pop(index)
 						open_states.append(child)
 				elif exists_in_open == False and exists_in_closed == True:
 					#child exists in closed states
-					if child.heuristic_value < closed_states[index].heuristic_value:
+					if child.cost < closed_states[index].cost:
 						closed_states.pop(index)
 						open_states.append(child)
 				else:
@@ -169,36 +224,39 @@ def best_first_search(start, goal, choice):
 		closed_states.append(current)
 		#Sort the open states by heuristic merit
 		open_states = in_place_selection_sort(open_states)
-		open_states.reverse()
-		print("open states -----------")
-		for state in open_states:
-			print_state(state.state)
-			print ("h-val = %i" % state.heuristic_value)
-		print ("End of iteration---------")
+		#print("open states -----------")
+		#for state in open_states:
+		#	print_state(state.state)
+		#	print ("h-val = %i" % state.cost)
+		#print ("End of iteration---------")
 
 	return None
+
 
 def print_state(node):
 	print (node[0], node[1], node[2])
 	print (node[3], node[4], node[5])
 	print (node[6], node[7], node[8])
 
-def init_node(state, parent, direction, depth, heuristic_value):
+
+def init_node(state, parent, direction, depth, cost):
 	#Node and all its values are sent in to this method and then a Node object is returned
 	#with all the values put together
-	return Node(state, parent, direction, depth, heuristic_value)
+	return Node(state, parent, direction, depth, cost)
+
 
 class Node:
-	def __init__(self, state, parent, direction, depth, heuristic_value):
+	def __init__(self, state, parent, direction, depth, cost):
 		#This class object is used to hold all the values necessary for each node.
 		self.state = state
 		self.parent = parent
 		self.direction = direction
 		self.depth = depth
-		self.heuristic_value = heuristic_value
-
+		self.cost = cost
+	
 		
 def main():
+	choice = 0
 	# Setting up a value to pass in through the terminal, to decide which heuristic to used.
 	if int(sys.argv[1]) > 0 and int(sys.argv[1]) < 4 and len(sys.argv) == 2:
 		choice = int(sys.argv[1])
@@ -212,11 +270,13 @@ def main():
 	elif choice == 2:
 		print("You selected the sum of distances out of place heuristic")
 	elif choice == 3:
-		pass
-	#Initiating start state for 8-puzzle by shuffling the values of the start state.
-	#Book example in page 141
-	start = [2, 8, 3, 1, 6, 4, 7, 0, 5]
-	#start = [1,2,3,8,0,4,7,6,5]
+		print("You selected a heuristic that takes sum of the distances out of place.")
+		print("It also tracks for any tile reversals that may occur.")
+	# Initiating start state for 8-puzzle by shuffling the values of the start state.
+	# Book example in page 141
+	start = [2, 8, 3, 1, 6, 4, 0, 7, 5]
+
+	#start = [1, 2, 3, 8, 0, 4, 7, 6, 5]
 	random.shuffle(start)
 
 	#Initiating the goal state of the 8-puzzle
@@ -226,12 +286,13 @@ def main():
 	print("Please wait.....")
 	result = best_first_search(start, goal, choice)
 
+	print ("================Results==============")
 	for res in result:
-		print("-------Results---------")
+		print("-------------------------")
 		print("Depth = %i" % res.depth)
-		print("HV = %i" % res.heuristic_value)
+		print("HV = %i" % res.cost)
 		print_state(res.state)
-		print("")
+		print("-------------------------")
 
 		
 if __name__ == '__main__':
